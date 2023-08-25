91c5c2a725d551a7f366d611a0f913f4685e753b
Lohuama <limalorruama98@gmail.com>
2023-08-25 13:59:03 -0300

    ibc

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,102 @@
+import React, { useContext } from "react";
+import "./Intro.css";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import boy from "../../img/boy.png";
+import culto from "../../img/culto.png";
+import youtube from "../../img/youtube.jpeg";
+import logo_ibc from "../../img/logo_ibc.png";
+import humble from "../../img/humble.png";
+import heartemoji from "../../img/heartemoji.png";
+import FloatinDiv from "../FloatingDiv/FloatingDiv";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import vetorAzul from "../../img/vetorAzul.png";
+import Instagram from "../../img/instagram.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+const Intro = () => {
+  // Transition
+  const transition = { duration: 2, type: "spring" };
+
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="Intro" id="Intro">
+      {/* left name side */}
+      <div className="i-left">
+        <div className="i-name">
+          {/* yahan change hy darkmode ka */}
+          <span style={{ color: darkMode ? "white" : "" }}>Bem-vindos à Igreja Batista Comunhão</span>
+          <span style={{display: 'none'}}>IBC</span>
+          <span>
+          Somos uma comunidade dedicada a compartilhar o amor do Evangelho. Nosso foco é amar a Deus e ao próximo. Junte-se a nós para cultos inspiradores, estudos bíblicos e projetos de serviço à comunidade. Seja parte da nossa família enquanto crescemos espiritualmente e impactamos vidas em João Pessoa.
+          </span>
+        </div>
+        <Link to="contact" smooth={true} spy={true}>
+          <button className="button i-button">Endereço</button>
+        </Link>
+        {/* social icons */}
+        <div className="i-icons">
+          <img src={youtube} alt="" />
+          <img src={Instagram} alt="" />
+        </div>
+      </div>
+      {/* right image side */}
+      <div className="i-right"> 
+        <img src={Vector2} alt="" />
+        <img src={vetorAzul} alt="" />
+        
+        <img src={logo_ibc} alt="" />
+        {/* animation */}
+        {/* <motion.img
+          initial={{ left: "-36%" }}
+          whileInView={{ left: "-24%" }}
+          transition={transition}
+          src={glassesimoji}
+          alt=""
+        /> */}
+
+        <motion.div
+          initial={{ top: "-4%", left: "74%" }}
+          whileInView={{ left: "68%" }}
+          transition={transition}
+          className="floating-div"
+        >
+          {/* <FloatinDiv img={humble} text1="Domingo" text2="18h00" /> */}
+        </motion.div>
+
+        {/* animation */}
+        <motion.div
+          initial={{ left: "9rem", top: "18rem" }}
+          whileInView={{ left: "0rem" }}
+          transition={transition}
+          className="floating-div"
+        >
+          {/* floatinDiv mein change hy dark mode ka */}
+          {/* <FloatinDiv img={heartemoji} text1="Louvor e " text2="Adoração" /> */}
+        </motion.div>
+
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,111 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './resume.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>Conheça Nossos Grupos: </span>
+        <span>Cresça Conosco!</span>
+        <spane>
+        Em nossa jornada de fé, a caminhada em grupo fortalece e enriquece. Na Igreja Batista Comunhão, 
+        <br />
+        Oferecemos uma variedade de grupos que o ajudarão a crescer espiritualmente e a construir relacionamentos significativos.
+        <br />
+        De grupos de estudo bíblico a encontros de oração e atividades de serviço comunitário,
+        <br />
+        há um lugar para você se conectar, aprender e compartilhar. 
+        <br />
+        Junte-se a um de nossos grupos e embarque em uma jornada de crescimento mútuo.
+        </spane>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Grupo de Louvor Comunhão"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "10rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Adejuc"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "13rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Ministério Infantil Comunhão"}
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+
+        {/* 4° card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "23rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Ministério Feminino Comunhão"}
+          />
+        </motion.div>
+        {/* 5º */}
+        <motion.div
+          initial={{ top: "27rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Ministério Casais Comunhão"}
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
